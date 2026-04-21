799da6da1e9c10c7d53cf8fa708f08ff0ea39405
JENTAH REHEMA <jentahrehema7@gmail.com>
2026-04-21 05:28:43 +0300

    chore: stop tracking backend build output

--- backend/dist/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { auth, authorizeRoles } from "../middleware/auth.js";
-import { createOrder, updateOrderStatus, getOrders, } from "../controller/orderController.js";
-const router = express.Router();
-router.get("/", auth, authorizeRoles("admin"), getOrders);
-router.post("/", createOrder);
-router.patch("/:id/orderStatus", auth, authorizeRoles("admin"), updateOrderStatus);
-export default router;
